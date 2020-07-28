6cb6b6702aaa05ed24f1f703dabeb26ccaf7c64c
2waybene <jianying.li@gmail.com>
2020-07-28 10:06:51 -0400

    trying

--- ui.R
+++ ui.R
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 options(warn=-1)
 source("helpers.R")
 
@@ -49,55 +49,3 @@ fluidPage(
 ) #end taglist
 )
 )
-=======
-options(warn=-1)
-source("helpers.R")
-
-fluidPage(
-           
-  titlePanel("Signature Analysis"),
-  
-  
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(width=4,
-      div(style="display: inline-block;vertical-align:top; width: 275px;",fileInput("fileSignature","Upload the signature file")), # Sig file input
-      div(style="display: inline-block;vertical-align:top; width: 10px;",HTML("<br>")),
-      div(style="display: inline-block;vertical-align:top; width: 100px;",radioButtons("radioGene", label="Gene Type",choices = list("Mouse" = 1, "Human" = 2),selected = 1)), # Mouse/Human choice
-      br(),
-      conditionalPanel("output.sigUploaded",div(style = 'height: 200px;margin: auto; overflow-y: scroll',DT::dataTableOutput("Sig",width = "100%"))), # Display sig only after input
-      br(),
-      div(style="display: inline-block;vertical-align:top; width: 275px;",fileInput("fileArray","Upload the human microarray file")), # Array file input
-      br(),
-      conditionalPanel("output.arrayUploaded",div(style = 'overflow-x: scroll',DT::dataTableOutput("Array",width = "100%"))), # Display array only after input
-      br(),
-      actionButton("goButton", "Go!",
-                   style="font-weight: bold; color: #00FF08; background-color: #aea79f; border-color: #aea79f")
-      
-      
-    
-  ),
-  tagList(
-  mainPanel(
-  navbarPage( # Theme and tabs
-    
-    theme = "bootstrap.min.cerulean.css",
-    title = "Tabs:",
-      
-      source("ui-Tscore.R",local=TRUE)$value,
-      source("ui-bootstrap.R",local=TRUE)$value,
-
-      source("ui-sem.R",local=TRUE)$value,
-      #source("ui-sem-dev.R",local=TRUE)$value,
-      #source("ui-dynamic.R",local=TRUE)$value,
-
-      source("ui-instructions.R",local=TRUE)$value
-    
-      
-    )
-  ) #end navbarpage
-) #end taglist
-)
-)
->>>>>>> 4e6687768090ca52b17ad68ffa5b2d22046aafba
