da25a2c64fc15e0d1560652e448690d510d3527e
Li, Jianying (NIH/NIEHS) [C] <jianying.li@nih.gov>
2020-07-28 13:45:30 -0400

    added file

--- ui-sem-dev.R
+++ ui-sem-dev.R
@@ -16,70 +16,22 @@ endoVars <- c(colnames(dat)) # Get variables
 
 tabPanel("SEM", 
          br(),
-         fluidRow(
-           tabsetPanel(
-             tabPanel("Model", 
-                      
-                    #  fluidRow(
-                    #      column(15, downloadButton ("downloadData", "Download SEM Report"))
-                     #           ),
-                          
-                      fluidRow(
-                        #  column(3, downloadButton ("downloadData", "Download SEM Report")
-                         #        ),
-                        
-                        
-                          column(6, # Drop down menus
-                          selectInput("exo1",label="Choose a exogenous variable", choices=endoVars, selected=endoVars[1]), 
-                          selectInput("exo2",label="Choose a exogenous variable", choices=endoVars, selected=endoVars[2]),
-                          selectInput("endo",label="Choose a endogenous variable", choices=endoVars, selected=endoVars[12])
-                        #  downloadButton ("downloadData", "Download SEM Report")
-                          ), 
-                         #),
-                      #  fluidRow( 
-                      #    column(5,div(style="display: inline-block;vertical-align:top;",verbatimTextOutput("semSummary"))),
-                       
-                        # downloadButton ("downloadData", "Download SEM Report")
-                        #  ),
-                          column(4, # SEM Model image
-                             div(style="display: inline-block;vertical-align:top;",imageOutput("semModel"))
-                          )
-                      ),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    fluidRow( 
-                      column(6,div(style="display: inline-block;vertical-align:top;",verbatimTextOutput("semSummary")))
-                      
-                      # downloadButton ("downloadData", "Download SEM Report")
-                   
-                    #       ),
-                    
-                    ##  can't add download button here!!
-                    #fluidRow(
-                    # downloadButton ("downloadData", "Download SEM Report")
-                               )
-                    ##  need help here!!
-                    
- 
-             ),
-             tabPanel("SEM Intro",includeMarkdown("instructions/SEMIntro.Rmd")) # Intro markdown file
-           )
+         
+         tabsetPanel(
+           tabPanel("Model", 
+                    fluidRow(
+                      column(3, # Drop down menus
+                             selectInput("exo1",label="Choose a exogenous variable", choices=endoVars, selected="GATA2_act_FC13_P01"), 
+                             selectInput("exo2",label="Choose a exogenous variable", choices=endoVars, selected="PRG_act_FC13_P01"),
+                             selectInput("endo",label="Choose a endogenous variable", choices=endoVars, selected="SOX17_lev"),
+                             downloadButton("semdownload", "Download Zip",
+                                            style="font-weight: bold;color: #000000; background-color: #F17F2B; border-color: #aea79f")),
+                      column(5,div(style="display: inline-block;vertical-align:top;",verbatimTextOutput("semSummary")))),
+                    column(4, # SEM Model image
+                           div(style="display: inline-block;vertical-align:top;",imageOutput("semModel")))
+           ),
+           tabPanel("SEM Intro",includeMarkdown("instructions/SEMIntro.Rmd")) # Intro markdown file
+           
          )
 )        
 
